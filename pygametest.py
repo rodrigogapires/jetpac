11471b5680fe1dcb1e2e9d71b69a71433bddfc5e
Rodrigo Pires <pires.rodrigo@outlook.com.br>
2019-10-23 14:52:40 -0300

    initial commit

---
+++ pygametest.py
@@ -0,0 +1,29 @@
+import pygame
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+pygame.init()
+
+screen = pygame.display.set_mode(
+    [1024, 768], pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE, 32)
+
+running = True
+while running:
+
+    screen.fill((255, 255, 255))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    pygame.display.flip()
+pygame.quit()
