bfd25323224e82ed5a7cda7a106bfbcb511fe4c5
Rodrigo Pires <54215336+rodrigogapires@users.noreply.github.com>
2019-11-06 13:51:30 -0300

    Update jetpac.py

--- jetpac.py
+++ jetpac.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-
+from player import Player
+from rocket import Rocket
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -14,20 +15,46 @@ from pygame.locals import (
 pygame.init()
 screen = pygame.display.set_mode(
     [256, 192], pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF, 32)
+pygame.mouse.set_visible(0)
 
-background = pygame.image.load("jetpac.png")
+background = pygame.image.load("sprites\\jetpac.png").convert_alpha()
+
+player = Player(130, 162)
+rocket = Rocket()
 
 running = True
-
+clock = pygame.time.Clock()
 while running:
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
 
-    screen.fill((255, 0, 0))
-    screen.blit(background, (0, 0))
+    player.gravity(clock.tick(30))
+    keys = pygame.key.get_pressed()
+    if keys[K_RIGHT]:
+        player.moveRight()
+    if keys[K_LEFT]:
+        player.moveLeft()
+    if keys[K_UP]:
+        player.moveUp()
+    if keys[K_DOWN]:
+        player.moveDown()
+
+    player.check()
+    #print(player.x, player.y)
+
+    rocket.getPart(player.x, player.y)
+
+    screen.blit(player.sprite, (player.x, player.y))
+    screen.blit(rocket.rocket1, (rocket.rocket1_x, rocket.rocket1_y))
+    screen.blit(rocket.rocket2, (rocket.rocket2_x, rocket.rocket2_y))
+    screen.blit(rocket.rocket3, (rocket.rocket3_x, rocket.rocket3_y))
     pygame.display.flip()
+    clock.tick(144)
 
 pygame.quit()
 sys.exit()
